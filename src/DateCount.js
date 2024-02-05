0f9d9f9176c453e04e75de2371c1932a531c5732
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2024-02-04 20:22:24 -0600

    I added the logic to the counter

--- src/DateCount.js
+++ src/DateCount.js
@@ -1,20 +1,60 @@
 import { useState } from "react";
 
 export default function DateCount() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(1);
+
+  function handleMinusStep() {
+    setStep((s) => s - 1);
+  }
+  function handleSumStep() {
+    setStep((s) => s + 1);
+  }
+  function handleMinusCount() {
+    setCount((c) => c - step);
+  }
+  function handleSumCount() {
+    setCount((c) => c + step);
+  }
+
   return (
     <>
-      {/* STEPS */}
+      {/********** STEPS ************/}
       <div style={{ display: "flex", color: "white" }}>
-        <button style={{ width: "40px", margin: "10px" }}>-</button>
-        <p>Step: 5</p>
-        <button style={{ width: "40px", margin: "10px" }}>+</button>
+        <button
+          onClick={handleMinusStep}
+          style={{ width: "40px", margin: "10px" }}
+        >
+          -
+        </button>
+        <p>
+          Step: <span>{step}</span>{" "}
+        </p>
+        <button
+          onClick={handleSumStep}
+          style={{ width: "40px", margin: "10px" }}
+        >
+          +
+        </button>
       </div>
 
-      {/* COUNT */}
+      {/*********** COUNT ************/}
       <div style={{ display: "flex", color: "white" }}>
-        <button style={{ width: "40px", margin: "10px" }}>-</button>
-        <p>Count: 25</p>
-        <button style={{ width: "40px", margin: "10px" }}>+</button>
+        <button
+          onClick={handleMinusCount}
+          style={{ width: "40px", margin: "10px" }}
+        >
+          -
+        </button>
+        <p>
+          Count: <span>{count}</span>
+        </p>
+        <button
+          onClick={handleSumCount}
+          style={{ width: "40px", margin: "10px" }}
+        >
+          +
+        </button>
       </div>
     </>
   );
