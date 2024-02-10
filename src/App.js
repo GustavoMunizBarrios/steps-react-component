771a51832f5f8658179c0331d443d481923608b2
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2024-02-09 23:11:05 -0600

    I made StepMessage element reusable

--- src/App.js
+++ src/App.js
@@ -38,9 +38,7 @@ export default function App() {
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
 
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
 
           <div className="buttons">
             <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
@@ -56,6 +54,15 @@ export default function App() {
     </>
   );
 }
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3> Step {step}</h3> {children}
+    </div>
+  );
+}
+
 function Button({ bgColor, textColor, onClick, children }) {
   // "children" is a predefine React keyword, this get has a prop everything between the tags of Button
   return (
